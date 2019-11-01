9b4087a6993e197e3c5356ea952934f3fdec734f
Dmirty Zyadik <zyadik@mail.ru>
2019-11-01 16:51:08 +0500

    011119

--- RWH/ch3/Ch3.hs
+++ RWH/ch3/Ch3.hs
@@ -76,12 +76,50 @@ heightTree :: Tree a -> Int
 heightTree Empty = 0
 heightTree (Node _ l r) = 1 + max (heightTree l) (heightTree r) 
 
+data Point2D = Point2D { x :: Double, y :: Double } deriving (Show)
 
-data Point = Point Float Float
+instance Eq Point2D where
+    (Point2D x1 y1) == (Point2D x2 y2) = y1 == y2 
+    (Point2D x1 y1) /= (Point2D x2 y2) = y1 /= y2 
+    
+instance Ord Point2D where    
+    (Point2D x1 y1) > (Point2D x2 y2) = (atan2 x1 y1) > (atan2 x2 y2)
+    (Point2D x1 y1) < (Point2D x2 y2) = (atan2 x1 y1) < (atan2 x2 y2)
+    (Point2D x1 y1) `compare` (Point2D x2 y2) = (atan2 x1 y1) `compare` (atan2 x2 y2)
 
-data Direction = Left' | Right' | Straight'
+data Direction = Left' | Right' | Straight' deriving (Show)
 
-angleSide :: Point -> Point -> Point -> Direction
-angleSide (Point ax ay) (Point bx by) (Point cx cy) | (ax > cx) && (cx > bx) = Right'
-angleSide (Point ax ay) (Point bx by) (Point cx cy) | (ax < cx) && (cx < bx) = Right'
-angleSide _ _ _ = Left'
+a = Point2D 1 2
+b = Point2D 2 1
+c = Point2D 3 2
+d = Point2D 2 3
+e = Point2D 3 4
+
+pointList = [a,b,c,d,e]
+
+lineAngle (Point2D x1 y1) (Point2D x2 y2) = atan2 (x2-x1)  (y2-y1)
+
+calcDirection :: Point2D -> Point2D -> Point2D -> Direction
+calcDirection p1 p2 p3
+    | la == ua = Straight'
+    | la > ua  = Left'
+    | otherwise = Right' 
+        where   la = lineAngle p1 p2 
+                ua = lineAngle p2 p3 
+                
+
+directionList :: [Point2D] -> [Direction]
+directionList [] = []
+directionList [a] = []
+directionList [a,_] = []
+directionList [a,b,c] = [(calcDirection a b c)]
+directionList (a:b:c:d) = (calcDirection a b c) : (directionList (b:c:d))
+
+pointsPairsSort pairs = sortBy comparison pairs
+                            where comparison (angleA, distanceA, _) (angleB, distanceB, _) 
+                                                | angleA > angleB = GT
+                                                | angleA < angleB = LT
+                                                | True = compare distanceA distanceB
+
+
+
